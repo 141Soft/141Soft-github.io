8b6aead6eaa450ff12f9dec8d0b7fb1e778fb268
141Soft <141Soft@proton.me>
2024-09-06 12:25:00 +0100

    Implements light/dark mode

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import Alpine from "alpinejs";
 
+document.addEventListener('alpine:init', () => {
+    Alpine.data('utils', ()=> ({
+        isDark: true,
+        toggle(){
+            this.isDark === true ? this.isDark = false : this.isDark = true;
+            document.body.classList.toggle('light-mode');
+        }
+    }))
+})
+
+
 window.Alpine = Alpine;
 
 Alpine.start();
